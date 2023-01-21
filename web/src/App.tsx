ca67d7cd0fafb64ff4efad0aaa6225c101300206
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-01-20 21:43:02 -0300

    Aula 02

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,18 @@
-import { Habit } from "./components/Habit";
+// import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
 import "./styles/global.css";
 
 export function App() {
   return (
-    <div>
-      <Habit completed={32} />
-      <Habit />
-      <Habit completed={64}/>
-      <Habit />
-      <Habit completed={128}/>
-      <Habit />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   );
 }
 
 export default App;
-
